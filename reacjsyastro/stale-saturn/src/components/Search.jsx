79a32a5f11d49974bd1ffdcb89113ce0e1f7fa09
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-10-21 11:02:08 -0500

    version astro reacjs

--- reacjsyastro/stale-saturn/src/components/Search.jsx
+++ reacjsyastro/stale-saturn/src/components/Search.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
+
+
 function Search() {
-  const [search, setSearch] = useState("hola");
+  const [search, setSearch] = useState("");
+  const buscar = (e) => setSearch(e.target.value);
+  const url="/results/"
   return (
     <>
       <div>
-        <h1>{search}</h1>
-        <button onClick={() => setSearch("cambio")}>cambio</button>
+        <form className="d-flex" action={url+search} method="post" >
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            value={search}
+            onChange={buscar}
+          />
+          <button className="btn btn-outline-success mx-4" type="submit">
+            Search
+          </button>
+        </form>
       </div>
     </>
   );
